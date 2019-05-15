9ac6fdeae834f2cfa29118f7b24ea16bd0c0f066
Kevin Hinds <khinds10.work@gmail.com>
2019-05-15 16:25:24 -0400

    get the basic code in place

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # PowerHub
 Central data persistence hub for internet enabled light and outlets in your home
+
+## API
+
+	http://myhost/message
+		(get the current message set)
+
+	http://myhost/message/set
+		(HTTP POST a raw string value to this URL to set a new message)
+	
+	http://myhost/flag/{id}
+		(get the current boolean status of the flag by integer: {id})
+
+	http://myhost/flag/all
+		(get all the current boolean status of all the flags as an array)
+
+	http://myhost/flag/{id}/set
+		(set the current boolean status to 'true' for the flag by integer: {id})
+
+	http://myhost/flag/{id}/unset
+		(set the current boolean status to 'false' for the flag by integer: {id})
+	
+	http://myhost/reading/{id}
+		(get the current averaged value for the reading by integer: {id})
+
+	http://myhost/reading/all
+		(get the current averaged value for all the readings as an array)
+
+	http://myhost/reading/{id}/set
+		(HTTP POST a raw numeric value to this URL to add a new value to the current calculated average)  
+			-- see below for how many values in total are compiled to the average value
+
+## Configuation
+
+The 'readings' values are calculated as averages of a certain number of recent persisted reading numeric values. 
+Set the following constant to how many of the most recent readings should be included to produce the average.
+
+***$readingsAverageLimit = 5;***
+
+## Datastore	
+Server will persist values to simple files located by naming conventions below. 
+Note: {id} will be replaced by the real integer presented by the incoming request's URL.
+
+	$valueFileFolder = 'values' (name of the folder to contain the measurement files)
+	$messageFileName = 'message.msg' (name of the message text file)
+	$readingsFilesNames = 'reading{id}.avg' (name of the CSV averaged readings file)
+	$flagFilesNames = 'flag{id}.flg' (name of the boolean flag value flag file)
---
+++ index.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * RetroDashboard Central Data Hub
+ *  This is a small PHP script to place on a webhost of your choosing. 
+ *  It has a simple API to get and set information to it. 
+ *  Note: Don’t forget to include the .htaccess file for proper URL routing to take place.
+ *
+ * @author khinds
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+/**
+ * check if a file is missing then create it if needed
+ * @param $fileName
+ */
+function createFileIfMissing($fileName) {
+	if (!file_exists($fileName)) touch($fileName);
+}
+
+/**
+ * for any possible malicious behavior, 
+ * 	sanitize the input from the GET / POST requests
+ * @param $value
+ */
+function cleanInput($value) {
+	return preg_replace("/[^a-zA-Z0-9\-\. ]/", "", $value);
+}
+
+/**
+ * generate a file name with the given id inserted to the file name's provided template
+ * @param $fileNameTemplate
+ * @param $id
+ */
+function generateFileById($fileNameTemplate, $id) {
+    $fileName = str_replace("{id}", $id, $fileNameTemplate);
+    createFileIfMissing($fileName);
+	return $fileName;
+}
+
+/**
+ * for a given filename if the value is empty, the return a "default" value
+ * @param $fileName
+ * @param $default
+ */
+function getValueOrDefault($fileName, $default) {
+	$value = file_get_contents($fileName);
+	if (empty($value)) $value = $default;
+	return $value;
+}
+
+/**
+ * for a given filename if the value is empty, the return a "default" value
+ * 	otherwise get the average of the last N number of readings
+ * @param $fileName
+ * @param $readingsAverageLimit
+ * @param $default
+ */
+function getAverageOrDefault($fileName, $readingsAverageLimit, $default) {
+	$valuesArray = preg_split("/,/", getValueOrDefault($fileName, $default));
+	$total = 0;
+	foreach ($valuesArray as $value) $total += $value;
+	return round($total/$readingsAverageLimit, 0);
+}
+
+// set the basic JSON response with the default response object
+header('Content-type: application/json');
+$response = new stdClass();
+$response->message = 'error: invalid request';
+
+// define file names to set for values on the server
+$valueFileFolder = 'values';
+$messageFileName = $valueFileFolder.'/message.msg';
+$readingsFilesNames = $valueFileFolder.'/reading{id}.avg';
+$flagFilesNames = $valueFileFolder.'/flag{id}.flg';
+
+// set readings average limit, the number of entries saved for each average value to be calculated against
+$readingsAverageLimit = 5;
+
+// process incoming request to map to the following actions below
+$urlHost = $_SERVER['HTTP_HOST'];
+$requestURI = $_SERVER['REQUEST_URI'];
+$urlParts = parse_url("http://{$urlHost}{$requestURI}");
+
+// get dashboard option
+preg_match('/\/[a-zA-Z]+\/?/', $urlParts['path'], $matches);
+$dashboardOption = '';
+if (isset($matches[0])) $dashboardOption = trim($matches[0], '/');
+
+// get id number for reading or flag if present
+preg_match('/\/[0-9]\/?/', $urlParts['path'], $matches);
+$idNumber = 0;
+if (isset($matches[0])) $idNumber = trim($matches[0], '/');
+
+// if we have a 'all' matched in the URL means, we should get ALL results for flag/reading
+$getAll = false;
+preg_match('/all/', $urlParts['path'], $matches);
+if (isset($matches[0])) $getAll = true;
+
+// get action value 'set'/'unset' for reading or flag if present
+preg_match('/\/(set)|(unset)\/?/', $urlParts['path'], $matches);
+$action = '';
+if (isset($matches[0])) $action = trim($matches[0], '/');
+
+// switch on the type of incoming request
+switch ($dashboardOption) {
+
+    case 'message':
+    
+	    createFileIfMissing($messageFileName);
+		if ($action == 'set') {
+			file_put_contents($messageFileName, cleanInput(file_get_contents("php://input")));
+			$response->message = 'message contents has been updated';
+			break;
+		}
+		$response->message = getValueOrDefault($messageFileName, '');
+        break;
+        
+    case 'reading':
+    
+        // get/set individual reading
+    	if (!empty($idNumber)) {
+    		$readingsFile = generateFileById($readingsFilesNames, $idNumber);
+			if ($action == 'set') {
+			
+				// keep a list of the last "readingsAverageLimit" number of values to later get the average of them
+				$existingValuesArray = preg_split("/,/", getValueOrDefault($readingsFile, '0'));
+				$existingValuesArray[] = cleanInput(file_get_contents("php://input"));
+				file_put_contents($readingsFile, implode(',',array_slice($existingValuesArray, -$readingsAverageLimit, $readingsAverageLimit)));
+				$response->message = 'reading value has been added to average';
+				break;
+			}
+			$response->message = getAverageOrDefault($readingsFile, $readingsAverageLimit, '0');
+		}
+		
+        // get all readings
+		if ($getAll) {
+    		$response->message = array();
+		    for ($count=1; $count<5; $count++) {
+                $readingsFile = generateFileById($readingsFilesNames, $count);
+                $response->message[] = getAverageOrDefault($readingsFile, $readingsAverageLimit, '0');
+		    }
+		}
+        break;
+
+    case 'flag':
+    
+        // get/set individual flag
+    	if (!empty($idNumber)) {
+    		$flagFile = generateFileById($flagFilesNames, $idNumber);
+    		
+			// set boolean value of true/false based on incoming request URL
+			if ($action == 'set' || $action == 'unset') {
+				$flagValue = ($action == 'set') ? '1' : '0';
+				file_put_contents($flagFile, $flagValue);
+				$response->message = 'flag has been set';
+				break;
+			}
+			$response->message = getValueOrDefault($flagFile, '0');
+		}
+		
+        // get all flags
+		if ($getAll) {
+    		$response->message = array();
+		    for ($count=1; $count<5; $count++) {
+                $flagFile = generateFileById($flagFilesNames, $count);
+                $response->message[] = getValueOrDefault($flagFile, '0');
+		    }
+		}
+        break;
+}
+
+print json_encode($response);
